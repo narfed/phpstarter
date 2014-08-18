9f168acd363f80c5542a71988d924886a16fbb15
narfed <narfed101@hotmail.com>
2014-08-18 09:59:59 -0400

    Starter site configured for webmatrix and mysql db on local pc

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms894');
+    define('DB_USER', 'simplecmsuser894');
+    define('DB_PASSWORD', 'o3cWa&&jBQ%?');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'innisfil1!');
+?>
